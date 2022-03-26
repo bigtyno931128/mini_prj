2adb867e396bc89c2b6ebc4e88d4b22e82dc8432
bigtyno931128 <bigtyno4@gmail.com>
2022-03-27 00:00:10 +0900

    회원가입 유효성 검사 + 중복처리

---
+++ src/main/java/com/sparta/mini_prj/models/KakaoUserInfoDto.java
@@ -0,0 +1,12 @@
+package com.sparta.mini_prj.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class KakaoUserInfoDto {
+    private Long id;
+    private String nickname;
+    private String email;
+}
